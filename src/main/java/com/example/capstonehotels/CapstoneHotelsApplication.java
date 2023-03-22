fbdf6bd369e3d7b416265048071def41bbaa7cde
Perryjay30 <pelumijsh@gmail.com>
2023-03-21 14:55:28 -1200

    Created the project

---
+++ src/main/java/com/example/capstonehotels/CapstoneHotelsApplication.java
@@ -0,0 +1,13 @@
+package com.example.capstonehotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneHotelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneHotelsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/capstonehotels/CapstoneHotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstonehotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneHotelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
