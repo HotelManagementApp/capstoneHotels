1bd4345cf9368c4bebba1e4b7632c53cd6c984d0
Perryjay30 <pelumijsh@gmail.com>
2023-03-25 20:09:44 -1200

    guestServiceTest

--- src/test/java/com/example/capstonehotels/services/GuestServiceImplTest.java
+++ src/test/java/com/example/capstonehotels/services/GuestServiceImplTest.java
@@ -3,6 +3,7 @@ package com.example.capstonehotels.services;
 import com.example.capstonehotels.data.model.RoomType;
 import com.example.capstonehotels.dtos.request.BookRoomRequest;
 import com.example.capstonehotels.dtos.response.Response;
+import com.example.capstonehotels.utils.exceptions.CapstoneException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,6 +31,21 @@ class GuestServiceImplTest {
         assertEquals("Your room has been booked successfully, Kindly proceed to " +
                 "the payment section", response.getMessage());
     }
+
+    @Test
+    void testThatGuestCanBookRoomThrowsException() {
+        BookRoomRequest bookRoomRequest = new BookRoomRequest();
+        bookRoomRequest.setRoomType(RoomType.SINGLE);
+        bookRoomRequest.setLastName("DrinkWater");
+        bookRoomRequest.setFirstName("Samuel");
+        bookRoomRequest.setCheckinDate("28-03-2023");
+        bookRoomRequest.setCheckoutDate("30-03-2023");
+        bookRoomRequest.setTelephoneNumber("+234 (563) 450-1257");
+        bookRoomRequest.setEmailAddress("dsfdadfdfds");
+        assertThrows(CapstoneException.class, () -> guestService.makeRoomReservation(bookRoomRequest));
+    }
+
+
 
 
 }
